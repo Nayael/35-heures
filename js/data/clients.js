af9389d3adf3ac6b09a156a50ecfb8c5f4fd7a42
Orphidios <ripoche.sylvain.isart@gmail.com>
2014-04-15 20:37:34 +0200

    Scenario

--- js/data/clients.js
+++ js/data/clients.js
@@ -3,81 +3,93 @@ var clients = (function() {
 
 	var clients = {
 		"Pro": {
-			"Name" : "Henry",
-			"StartPatience" : 60,		
-			"PhraseIntro" : "Bonjour, je viens retirer une commande, tenez voici le recommandé ainsi que ma carte d’identité.",
-			"Vulnerability" : {
-								"technology" : 0.5,
-								"paperwork" : 1.5
-							  },
-			"Action" : {
-								"phaseOne" :{
-									"action" : "computerFakbok",
-									"phrase" : "Vous faites quoi là !?",
-									"result" : false
-								},
-								"checkPapers" :{
-									"phrase" : "Ce sont les bons papiers, pas la peine de vérifier",
-									"success" : false
-								},
-								"getPackage" :{
-									"phrase" : "Merci, bonne journée",
-									"success" : true
-								},
-								"phaseDefault" : {
-									"phrase" : "Vous croyez que c'est le moment de faire ça",
-									"success" : false
-								}
+			"Name": "Henry",
+			"StartPatience": 60,
+			"PhraseIntro": "Bonjour, je viens retirer une commande, tenez voici le recommandé ainsi que ma carte d’identité.",
+			"Vulnerability": {
+				"technology": 0.5,
+				"paperwork": 1.5
+			},
+			"Scenario": {
+				"default": {
+					"computerFakbok": {
+						"phrase": "Vous faites quoi là !?"
+					},
+					"phaseDefault": {
+						"phrase": "Vous croyez que c'est le moment de faire ça"
+					}
+				},
+				"phase_0": {
+					"intro": "Je viens retirer une commande! tenez voici le recommandé ainsi que ma carte d’identité. et vite ...SVP!",
+					"success": "checkPapers",
+					"checkPapers": {
+						"phrase": "Tenez voici mes papier"
+					},
+					"getPackage": {
+						"phrase": "Vous ne vérifier pas mon identité"
+					}
 
+				},
+				"phase_1": {
+					"intro": "Bon maintenant que vous avez vérifier mes papiers je veux bien ma commande",
+					"success": "getPackage",
+					"checkPapers": {
+						"phrase": "Je vous est déjà donner les papiers"
+					},
+					"getPackage": {
+						"phrase": "Merci, bonne journée"
+					}
+				},
+				"fail": "Bon je pense que vous n'y mettez pas du votre! j'en parlerais à vos supérieurs, je ne vous souhaite pas le bonsoir monsieur"
 			},
 		},
 		"OldMan": {
-			"Name" : "Jean-Jacques",
-			"Phrases" : {	"type" : "technology",
-							"phrase" : "Bonjour, je m’en occupe tout de suite."
-						},
-			"Patience" : 100
-			"Vulnerability" : {
-								"technology" : 3,
-								"paperwork" : 0.8
-							  },
-			"Action" : {
-								"computerFakbok" : {
-									"phrase" : ""
-									"success" : true
-								},
-								"computerInfo" : {
-									"phrase" : ""
-									"success" : true
-								},
-								"computerKey" : {
-									"phrase" : ""
-									"success" : true
-								},
-								"stampFail" :{
-									"phrase" : ""
-									"success" : false
-								},
-								"closeFail" :{
-									"phrase" : ""
-									"success" : false
-								},
-								"tamponWrong" :{
-									"phrase" : ""
-									"success" : false
-								},
-								"tamponFail" :{
-									"phrase" : ""
-									"success" : false
-								},
-								"staplerEmpty" :{
-									"phrase" : ""
-									"success" : false
-								},
-								"default" : {
-									"phrase" : "J'ai pas envie de pourrir ici moi !",
-									"success" : false
-								}
+			"Name": "Jean-Jacques",
+			"Phrases": {
+				"type": "technology",
+				"phrase": "Bonjour, je m’en occupe tout de suite."
+			},
+			"Patience": 100 "Vulnerability": {
+				"technology": 3,
+				"paperwork": 0.8
+			},
+			"Action": {
+				"computerFakbok": {
+					"phrase": ""
+					"success": true
+				},
+				"computerInfo": {
+					"phrase": ""
+					"success": true
+				},
+				"computerKey": {
+					"phrase": ""
+					"success": true
+				},
+				"stampFail": {
+					"phrase": ""
+					"success": false
+				},
+				"closeFail": {
+					"phrase": ""
+					"success": false
+				},
+				"tamponWrong": {
+					"phrase": ""
+					"success": false
+				},
+				"tamponFail": {
+					"phrase": ""
+					"success": false
+				},
+				"staplerEmpty": {
+					"phrase": ""
+					"success": false
+				},
+				"default": {
+					"phrase": "J'ai pas envie de pourrir ici moi !",
+					"success": false
+				}
 
 			},
 		}
