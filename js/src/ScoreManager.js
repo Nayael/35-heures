2a7f2ca078e7a9bf4dcd9b3170f21aab59c76786
Orphidios <ripoche.sylvain.isart@gmail.com>
2014-04-16 15:33:05 +0200

    Score + TimeManager

--- js/src/ScoreManager.js
+++ js/src/ScoreManager.js
@@ -12,6 +12,29 @@ var ScoreManager = (function() {
 			return new ScoreManager();
 		}
 
+		this.clients = [];
+		this.days = [];
+		this.weeks = [];
+	};
+
+	ScoreManager.prototype.endOfDay = function() {
+		var totalTime = 0;
+		var neededTime = 0;
+		for (var i = 0; i < this.clients.length; i++) {
+			totalTime += this.clients[i].totalTime;
+			neededTime += this.clients[i].neededTime;
+		}
+	};
+
+	ScoreManager.prototype.endOfWeek = function() {
+
+	};
+
+	ScoreManager.prototype.endOfClient = function(client) {
+		this.currClient.push({
+			totalTime: client.totalTime,
+			neededTime: client.neededTime
+		});
 	};
 
 	// Singleton
